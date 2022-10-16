0cd91bbe21e00df2d9efad494396f55de15f443d
Garvit <81004891+garvitcode@users.noreply.github.com>
2022-10-16 18:08:00 +0530

    Update TT.py
    Change the address of images according to you.

--- TT.py
+++ TT.py
@@ -1,151 +1,127 @@
+#----------------------------------------------------------IMPORTING FILES-----------------------------------------------------------------------#
 
-from tkinter import *
-from tkinter import ttk
-from tkinter import messagebox
-from textblob import TextBlob
+from tkinter import*
+from tkinter import ttk,messagebox
+from googletrans import Translator,LANGUAGES
 
-
-root = Tk()
- 
-root.title('TRANSLATOR')
-root.geometry('600x300')
-root.resizable(0,0)
- 
-root.config(bg='light green')
-root.iconbitmap(r'C:\\Users\\hp\\OneDrive\\Pictures\\translator.ico')
+#-----------------------------------------------------------------------#
 
 
-#------------------------------------------------------------------------------------------------------------------------------------------
+#----------------------------------------------------------CODE STARTS---------------------------------------------------------------------------#
 
 
-lan_dict = {'afrikaans': 'af', 'albanian': 'sq', 'amharic': 'am', 'arabic': 'ar', 'armenian': 'hy', 'azerbaijani': 'az', 'basque': 'eu', 'belarusian': 'be', 'bengali': 'bn', 'bosnian': 'bs', 'bulgarian': 'bg', 'catalan': 'ca', 'cebuano': 'ceb', 'chichewa': 'ny', 'chinese (simplified)': 'zh-cn', 'chinese (traditional)': 'zh-tw', 'corsican': 'co', 'croatian': 'hr', 'czech': 'cs', 'danish': 'da', 'dutch': 'nl', 'english': 'en', 'esperanto': 'eo', 'estonian': 'et', 'filipino': 'tl', 'finnish': 'fi', 'french': 'fr', 'frisian': 'fy', 'galician': 'gl', 'georgian': 'ka', 'german': 'de', 'greek': 'el', 'gujarati': 'gu', 'haitian creole': 'ht', 'hausa': 'ha', 'hawaiian': 'haw', 'hebrew': 'iw', 'hindi': 'hi', 'hmong': 'hmn', 'hungarian': 'hu', 'icelandic': 'is', 'igbo': 'ig', 'indonesian': 'id', 'irish': 'ga', 'italian': 'it', 'japanese': 'ja', 'javanese': 'jw', 'kannada': 'kn', 'kazakh': 'kk', 'khmer': 'km', 'korean': 'ko', 'kurdish (kurmanji)': 'ku', 'kyrgyz': 'ky', 'lao': 'lo', 'latin': 'la', 'latvian': 'lv', 'lithuanian': 'lt', 'luxembourgish': 'lb', 'macedonian': 'mk', 'malagasy': 'mg', 'malay': 'ms', 'malayalam': 'ml', 'maltese': 'mt', 'maori': 'mi', 'marathi': 'mr', 'mongolian': 'mn', 'myanmar (burmese)': 'my', 'nepali': 'ne', 'norwegian': 'no', 'pashto': 'ps', 'persian': 'fa', 'polish': 'pl', 'portuguese': 'pt', 'punjabi': 'pa', 'romanian': 'ro', 'russian': 'ru', 'samoan': 'sm', 'scots gaelic': 'gd', 'serbian': 'sr', 'sesotho': 'st', 'shona': 'sn', 'sindhi': 'sd', 'sinhala': 'si', 'slovak': 'sk', 'slovenian': 'sl', 'somali': 'so', 'spanish': 'es', 'sundanese': 'su', 'swahili': 'sw', 'swedish': 'sv', 'tajik': 'tg', 'tamil': 'ta', 'telugu': 'te', 'thai': 'th', 'turkish': 'tr', 'ukrainian': 'uk', 'urdu': 'ur', 'uzbek': 'uz', 'vietnamese': 'vi', 'welsh': 'cy', 'xhosa': 'xh', 'yiddish': 'yi', 'yoruba': 'yo', 'zulu': 'zu', 'Filipino': 'fil', 'Hebrew': 'he'}
+#----------------------------INITIATION OF ROOT-----------------------------------------#
+
+root=Tk()
+root.geometry('740x400')
+root.title('Translator')
+
+#-----------------------------------------------------------------------#
 
 
-#------------------------------------------------------------------------------------------------------------------------------------------
+#----------------------------FUNCTIONS--------------------------------------------------#
+
+def show():
+    c1=comb1.get()
+    c2=comb2.get()
+    lab1.config(text=c1)
+    lab2.config(text=c2)
+    root.after(1000,show)
+
+def translate():
+    trans=Translator()
+    transl=trans.translate(text1.get(1.0,END),src=comb1.get(),dest=comb2.get())
+
+    text2.delete(1.0,END)
+    text2.insert(END,transl.text)
+             
+def clear():
+    text1.delete(1.0,END)
+    text2.delete(1.0,END)
+
+def exite():
+    root.destroy()
+
+#---------------------------------------------------------------------#
 
 
-#--------------------------------------------------------------------------FUNCTIONS
+#----------------------------IMAGES------------------------------------------------------------#
+
+img=PhotoImage(file=r'C:\Users\Garvit\Desktop\code\Trans\BG.png')
+lab=Label(root,image=img)
+lab.pack()
+
+convert_img=PhotoImage(file=r'C:\Users\Garvit\Desktop\code\Trans\convert.png')
+
+clear_img=PhotoImage(file=r'C:\Users\Garvit\Desktop\code\Trans\cl.png')
+
+exit_img=PhotoImage(file=r'C:\Users\Garvit\Desktop\code\Trans\exts.png')
+
+#-----------------------------------------------------------------------#
 
 
-def main(event=None):
-    try:
-        word3 = TextBlob(varname1.get())
-        lan = word3.detect_language()
-        lan_todict = languages.get()
-        lan_to = lan_dict[lan_todict]
-        word3 = word3.translate(from_lang=lan,to=lan_to)
-        label3.configure(text=word3)
-        varname2.set(word3)
-    except:
-        varname2.set('try another keyword')
+#-----------------------------INCULUSION OF LANGUAGES------------------------------------------#
+
+language=list(LANGUAGES.values())
+
+#-----------------------------------------------------------------------#
 
 
-def diff_lans():
-    global languages
-    languages = StringVar()
-    font_box = ttk.Combobox(root,width=10,textvariable=languages,state='readonly')
-    font_box['values'] = [e for e in lan_dict.keys()]
-    font_box.current(37)
-    font_box.place(x=0,y=0)
+#-----------------------------LANGUAGE SELECTION BOX -------------------------------------------#
+
+lab1=Label(root,text='English',font=('Engravers ','30','bold'),bg='white',fg='#5582f9')
+lab1.place(x=90,y=30)
+comb1=ttk.Combobox(root,values=language)
+comb1.place(x=90,y=80)
+comb1.set('English')
 
 
-def about_us():
-    messagebox.showinfo('TRANSLATOR','This is a translation tool viz. build using Python Tkinter.')
+lab2=Label(root,text='Hindi',font=('Engravers ','30','bold'),bg='#5582f9',fg='white')
+lab2.place(x=500,y=35)
+comb2=ttk.Combobox(root,values=language)
+comb2.place(x=500,y=80)
+comb2.set('Hindi')
+
+#------------------------------------------------------------------------#
 
 
-#---------------------------------------------------------------------------CREATING_MENU_BAR
+#----------------------------TEXT BOX AREA ---------------------------------------------------------------#
+
+text1=Text(root,height=10,width=35,bg='#5582f9',fg='white',wrap=WORD)
+text1.place(x=30,y=100)
+
+text2=Text(root,height=10,width=35,bg='white',fg='#5582f9')
+text2.place(x=430,y=100)
+
+#-------------------------------------------------------------------------#
 
 
-menubar = Menu(root)
-root.config(menu=menubar)
+#-------------------------------------------BUTTONS-------------------------------------------------------#
+
+correct=Button(root,text='correct',command=translate,font=('arial 20'),bd=0, bg='white',image=convert_img,cursor='hand2')
+correct.place(x=43,y=300)
+
+clear=Button(root,text='clear',command=clear,font=('Times New Roman ','20'),bd=0,bg='#5582f9',image=clear_img,cursor='hand2')
+clear.place(x=300,y=300)
+
+ext=Button(root,text='exit',command=exite,font=('arial 20'),bd=0,bg='#5582f9',image=exit_img,cursor='hand2')
+ext.place(x=530,y=300)
+
+#-------------------------------------------------------------------------#
 
 
-#----------------------------------------------------------------------------CREATING_SUBMENU
+#---------------------------------FUNCTION TO SHOW TRANSLATED TEXT-------------------------------------------#
+
+show()
+
+#--------------------------------------------------------------------------#
 
 
-subMenu = Menu(menubar, tearoff = 0)
-
-menubar.add_cascade(label="Choose Output Languages",menu=subMenu)
-subMenu.add_command(label="AVAILABLE LANGUAGE", command=diff_lans)
-subMenu.add_command(label="Exit", command=root.destroy)
-
-subMenu = Menu(menubar,tearoff = 0)
-menubar.add_cascade(label="Help", menu=subMenu)
-subMenu.add_command(label="About Us", command=about_us)
-
-
-#-----------------------------------------------------------------------------ENTRY_BOX
-
-
-varname1 = StringVar()
-entry1 = Entry(root,width=30,textvariable=varname1,font=('times',20,'italic bold'))
-entry1.place(x=150,y=40)
-
-varname2 = StringVar()
-entry2 = Entry(root,width=30,textvariable=varname2,font=('times',20,'italic bold'))
-entry2.place(x=150,y=200)
-
-
-#-----------------------------------------------------------------------------LABELS
-
-
-label1 = Label(root,text='Latin Text  : ',font=('times',15,'italic bold'),bg='light green')
-label1.place(x=5,y=40)
-
-label2 = Label(root,text='Translated  : ',font=('times',15,'italic bold'),bg='light green')
-label2.place(x=5,y=200)
-
-label3 = Label(root,text='',font=('times',15,'italic bold'),bg='turquoise1')
-
-
-#-----------------------------------------------------------------------------BUTTONS
-
-
-btn = Button(root,text=' Translate ',bd=10,bg='yellow',activebackground= 'red',width='10',font=('times',15,'italic bold'),command=main)
-btn.place(x=287,y=110)
-
-root.bind('<Return>',main)
-
-
-#-----------------------------------------------------------------------------BINDING_FUNCTION
-
-
-def on_enterentry1(e):
-    entry1['bg'] = 'powder blue'
-def on_leaveentry1(e):
-    entry1['bg'] = 'white'
-
-def on_enterentry2(e):
-    entry2['bg'] = 'powder blue'
-def on_leaveentry2(e):
-    entry2['bg'] = 'white'
-
-def on_enterbtn(e):
-    btn['bg'] = 'red'
-def on_leavebtn(e):
-    btn['bg'] = 'yellow'
-
-
-#-----------------------------------------------------------------------------BINDING
-
-
-entry1.bind('<Enter>',on_enterentry1)
-entry1.bind('<Leave>',on_leaveentry1)
-
-entry2.bind('<Enter>',on_enterentry2)
-entry2.bind('<Leave>',on_leaveentry2)
-
-btn.bind('<Enter>',on_enterbtn)
-btn.bind('<Leave>',on_leavebtn)
-
-
-#-----------------------------------------------------------------------------STATUS_BAR
-
-
-statusbar = ttk.Label(root, text="Welcome to TRANSLATOR", relief=SUNKEN, anchor=W, font=('times',10))
-statusbar.pack(side=BOTTOM, fill=X)
-
-#----------------------------------------------------------------------------------------------------------------------------
-
+#--------------------------------------------DESTROY OF ROOT--------------------------------------------------#
 
 root.mainloop()
+
+#-----------------------------------------------------------------------#
+
+
+#---------------------------------------------------------END OF CODE----------------------------------------------------------------------------3
